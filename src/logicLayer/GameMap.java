b6393e88e6d9a0051e6f08627a5d8334bffc49d9
ljj <ljj@10.12.60.167>
2020-02-17 15:08:20 -0700

    Added constractor to GameMap enum for enemy and player has been established

--- src/logicLayer/GameMap.java
+++ src/logicLayer/GameMap.java
@@ -2,11 +2,22 @@ package logicLayer;
 
 public class GameMap
 {
-	private Tile[][] tile;
+	private Tile[][] tiles;
 	private int turnCount;
+	
+	public GameMap(Tile[][] tile)
+	{
+		tiles = tile;
+		turnCount=1;
+	}
 	
 	public void increaseTurn()
 	{
 		turnCount++;
+	}
+	
+	public Tile[][] getTiles()
+	{
+		return tiles;
 	}
 }
