abfa0c688587c59e769e5b259c7038c29284b107
Ibrahim Sultan <sultanibrahim01@gmail.com>
2020-02-17 14:44:33 -0700

    Fixed a couple of mistakes

--- src/logicLayer/Position.java
+++ src/logicLayer/Position.java
@@ -1,5 +1,20 @@
 package logicLayer;
 
-public interface Position {
-
+public class Position
+{
+	private int x,y;
+	public void setPos(int newx, int newy)
+	{
+		x=newx;
+		y=newy;
+	}
+	
+	public int getX()
+	{
+		return x;
+	}
+	public int getY()
+	{
+		return y;
+	}
 }
--- src/main/Coordinator.java
+++ src/main/Coordinator.java
@@ -1,4 +1,12 @@
 package main;
 
 public class Coordinator {
+	
+	public void parseStringInput(String userInput) {
+		
+	}
+	
+	private void performMoveToCommand(){
+		
+	}
 }
