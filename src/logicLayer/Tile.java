ee5570242a0350da8e0891b884b44bb7f78adebb
JackyL1103 <60748804+JackyL1103@users.noreply.github.com>
2020-02-17 14:30:14 -0700

    Update Tile.java
    Added getter and setter for unit and position

--- src/logicLayer/Tile.java
+++ src/logicLayer/Tile.java
@@ -2,6 +2,23 @@ package logicLayer;
 
 public class Tile 
 {
-	Unit unit;
-	Pos position;
+	private Unit unit;
+	private Pos position;
+	
+	public Unit getUnit()
+	{
+		return unit;
+	}
+	public Pos getPos()
+	{
+		return position;
+	}
+	public void setUnit(Unit newUnit)
+	{
+		unit=newUnit;
+	}
+	public void setPos(Unit newPos)
+	{
+		position=newPos;
+	}
 }
