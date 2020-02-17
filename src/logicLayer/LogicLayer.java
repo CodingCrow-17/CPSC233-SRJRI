b894746ca6a53b3d5a000cd70996c17d81f7ebad
Ibrahim Sultan <sultanibrahim01@gmail.com>
2020-02-17 15:33:10 -0700

    Worked on logic layer some more

--- src/logicLayer/LogicLayer.java
+++ src/logicLayer/LogicLayer.java
@@ -1,8 +1,26 @@
 package logicLayer;
 
-public interface LogicLayer {
-	void moveTo();
-	void performCombat();
-	void removeUnit();
-	void forecastCombat();
+public class LogicLayer{
+
+	public void moveTo(Position startPosition, Position endPosition) {
+		
+	}
+
+	public void performCombat(Position unitPosition, Position enemyPosition) {
+		
+	}
+
+	public void removeUnit() {
+		
+	}
+
+	public void forecastCombat() {
+		
+	}
+	
+	private boolean checkIfUnitIsAtPosition() {
+		
+		return true;
+	}
+	
 }
--- src/logicLayer/Owner.java
+++ src/logicLayer/Owner.java
@@ -1,5 +1,5 @@
 package logicLayer;
 
 public enum Owner {
-
+	
 }
